e422eba19c881605a7b439da5c53d8332f249225
varmab <varmab@gmail.com>
2017-01-02 10:50:13 +0530

    updated functions

--- javascript/variables/class.js
+++ javascript/variables/class.js
@@ -1,1 +1,23 @@
 console.log("Hello Variables");
+console.log("I am Varma");
+
+var myName = "Varma Bhupatiraju";
+console.log("I am " + myName);
+
+var myFirstName;
+
+myFirstName="Varma";
+console.log(myFirstName);
+
+var myLastName;
+console.log(myLastName);
+
+//var 1card="Spades"; //Error You can't start with number or special charecter
+
+//No special charecters allowed in variable names
+myLastName="Bhupatiraju";
+//+ operator
+var myFullName=myFirstName + ' ' + myLastName;
+console.log(myFullName);
+
+console.log(typeof(myFirstName));
